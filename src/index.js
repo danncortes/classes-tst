68ac1e3c10874a20fa28eab7c45e4148cc7f94a7
Daniel Cortes <danncortes@gmail.com>
2018-10-22 07:39:32 -0500

    Cleaning

--- src/index.js
+++ src/index.js
@@ -41,34 +41,3 @@ export class Quiz {
     this.qna = qna;
   }
 }
-
-const teachers = [
-  new Teacher(1, 'X professor'),
-  new Teacher(2, 'Logan James'),
-  new Teacher(3, 'Erik Lehnsherr'),
-];
-
-const students = [
-  new Student(1, 'Jhon'),
-  new Student(2, 'Peter'),
-  new Student(3, 'Anna'),
-  new Student(4, 'Rebecca'),
-  new Student(5, 'Tom'),
-  new Student(6, 'Rick'),
-];
-
-const classes = [
-  new Classes(1, 'Maths', 1, [1, 2, 3]),
-  new Classes(1, 'workout', 2, [3, 4, 5]),
-  new Classes(1, 'Physic', 3, [4, 5, 6]),
-];
-
-const quizzes = [];
-
-const qnaQuiz1 = [
-  new Qna('2 countries in america', ['Argentina', 'usa', 'italy', 'Rusia'], [0, 1]),
-  new Qna('2 countries in Asia', ['Chile', 'Nigeria', 'China', 'Japan'], [2, 3]),
-  new Qna('2 countries in Africa', ['Camerun', 'Brasil', 'China', 'Nigeria'], [0, 2]),
-];
-
-teachers[0].createQuiz(1, quizzes, 'Countries In continents', qnaQuiz1);
